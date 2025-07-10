350c32c77bca1765e5b8520b6da0200a6eddc85a
Josh-Weidner <josh.weidner2@gmail.com>
2025-07-09 22:49:22 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Josh Weidner on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
