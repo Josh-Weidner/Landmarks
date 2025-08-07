38a6b7edb4dac0e47a2164bc94f84a045a3ac583
Josh-Weidner <josh.weidner2@gmail.com>
2025-08-06 22:18:21 -0600

    Put together the complete badge, and now understand how drawing works.

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Josh Weidner on 8/6/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol()
+}
