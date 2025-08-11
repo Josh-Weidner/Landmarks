ef96be3b77cb0ab8118a9ed9a2a4e5483c89d185
Josh-Weidner <josh.weidner2@gmail.com>
2025-08-11 17:55:28 -0600

    hike graphic

--- AnimatingViewsAndTransitions/StartingPoint/Landmarks/Model/ModelData.swift
+++ AnimatingViewsAndTransitions/StartingPoint/Landmarks/Model/ModelData.swift
@@ -1,33 +1,31 @@
-//
-//  ModelData.swift
-//  Landmarks
-//
-//  Created by Josh Weidner on 7/24/25.
-//
-import Foundation
+/*
+See the LICENSE.txt file for this sample’s licensing information.
 
+Abstract:
+Storage for model data.
+*/
+
+import Foundation
 
 @Observable
 class ModelData {
     var landmarks: [Landmark] = load("landmarkData.json")
-    var hikes: [Hike] = load("hikeData.json")
 }
-
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
+
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
@@ -35,3 +33,4 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
